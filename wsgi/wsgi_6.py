d8e8f3b9d0e304de67441ec6562ae7a4f9509a99
Anitha <53578966+anithavumba@users.noreply.github.com>
2023-05-06 00:38:57 +0200

    Updates
    Update

--- wsgi/wsgi_6.py
+++ wsgi/wsgi_6.py
@@ -4,7 +4,8 @@ imports Flask instance for gunicorn configurations
 gunicorn --bind 127.0.0.1:8001 wsgi:web_flask.app
 """
 
-web_flask = __import__('web_flask.0-hello_route', globals(), locals(), ['*'])
+web_flask = __import__('web_flask.6-number_odd_or_even',
+                       globals(), locals(), ['*'])
 
 if __name__ == "__main__":
     """runs the main flask app"""
--- wsgi/wsgi_hbnb.py
+++ wsgi/wsgi_hbnb.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
 imports Flask instance for gunicorn configurations
-gunicorn --bind 127.0.0.1:8001 wsgi:web_flask.app
+gunicorn --bind 127.0.0.1:8003 wsgi.wsgi_hbnb:web_flask.app
 """
 
-web_flask = __import__('web_flask.0-hello_route', globals(), locals(), ['*'])
+web_flask = __import__('web_flask.100-hbnb',
+                       globals(), locals(), ['*'])
 
 if __name__ == "__main__":
     """runs the main flask app"""
