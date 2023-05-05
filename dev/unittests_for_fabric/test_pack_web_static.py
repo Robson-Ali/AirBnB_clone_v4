588c8cf32e3d6c0c6bcc6b8757bd021cf0a1bd60
Anitha <53578966+anithavumba@users.noreply.github.com>
2023-05-06 00:17:20 +0200

    Updates
    Updates

--- dev/unittests_for_fabric/test_pack_web_static.py
+++ dev/unittests_for_fabric/test_pack_web_static.py
@@ -8,10 +8,10 @@ import web_flask
 import unittest
 from os import stat
 import api
-module = __import__('3-deploy_web_static', globals(), locals(), ['*'])
+module = __import__('1-pack_web_static', globals(), locals(), ['*'])
 
 
-class TestDeployWebStaticDocs(unittest.TestCase):
+class TestPackWebStaticDocs(unittest.TestCase):
     """Class for testing Hello Route docs"""
 
     all_funcs = inspect.getmembers(module, inspect.isfunction)
@@ -20,7 +20,7 @@ class TestDeployWebStaticDocs(unittest.TestCase):
     def setUpClass(cls):
         print('\n\n.................................')
         print('..... Testing Documentation .....')
-        print('.......  DeployWebStatic API  .......')
+        print('.......  PackWebStatic API  .......')
         print('.................................\n\n')
 
     def test_doc_file(self):
@@ -30,19 +30,19 @@ class TestDeployWebStaticDocs(unittest.TestCase):
 
     def test_all_function_docs(self):
         """... tests for ALL DOCS for all functions"""
-        all_functions = TestDeployWebStaticDocs.all_funcs
+        all_functions = TestPackWebStaticDocs.all_funcs
         for function in all_functions:
             self.assertIsNotNone(function[1].__doc__)
 
     def test_pep8(self):
         """... tests if file conforms to PEP8 Style"""
         pep8style = pep8.StyleGuide(quiet=True)
-        errors = pep8style.check_files(['3-deploy_web_static.py'])
+        errors = pep8style.check_files(['1-pack_web_static.py'])
         self.assertEqual(errors.total_errors, 0, errors.messages)
 
     def test_file_is_executable(self):
         """... tests if file has correct permissions so user can execute"""
-        file_stat = stat('3-deploy_web_static.py')
+        file_stat = stat('1-pack_web_static.py')
         permissions = str(oct(file_stat[0]))
         self.assertEqual(permissions[5:], "775")
 
